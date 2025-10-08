f8a9195dbdfd64d031ae8bceca04cee3cbd4f051
Lucas Coelho Suero <137530955+LucasSuero@users.noreply.github.com>
2025-10-08 19:55:25 -0300

    Update ex1.php

--- ex1.php
+++ ex1.php
@@ -1,6 +1,19 @@
 <?php 
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 08 de Outubro de 2025
+Descritivo:FOR: Crie um programa que use for para imprimir os números pares de 2 a 20.
+*******************************************************************************/
     for ($n = 0; $n<=10; $n++) {
         echo "Número". $n . "\n" ;
     
 }
+
 ?>
