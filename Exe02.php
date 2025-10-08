61ceb309761aad7478545ee0d2a1c791cfb10152
Leonardo <leokenjits@gmail.com>
2025-10-08 19:50:36 -0300

    Exercicio 02

---
+++ Exe02.php
@@ -0,0 +1,18 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 08 de Outubro de 2025
+Descritivo:FOR: Crie um programa que use for para imprimir os números pares de 2 a 20.
+*******************************************************************************/
+for ($i = 2; $i <= 20; $i += 2) { //for usará um contador de 2 em 2 a partir do número 2 e parando no número 20.
+    echo $i."\n"; //imprime o contador
+}
+
+?>
