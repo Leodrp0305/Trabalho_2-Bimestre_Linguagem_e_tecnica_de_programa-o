6b79f018831fea79eb773fb80377242d92ab08a4
BorgesCesu <contato.eliasborges@gmail.com>
2025-10-08 20:29:25 -0300

    Add files via upload

---
+++ Exe15.php
@@ -0,0 +1,31 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 08 de Outubro de 2025
+Descritivo:FOR: Escreva um programa que use for para imprimir os números de 1 a 10.
+*******************************************************************************/
+echo "Números primos entre 1 e 50:\n";
+
+for ($num = 2; $num <= 50; $num++) {
+    $ehPrimo = true;
+
+    // Verifica se $num é divisível por algum número entre 2 e $num-1
+    for ($i = 2; $i <= sqrt($num); $i++) {
+        if ($num % $i == 0) {
+            $ehPrimo = false;
+            break;
+        }
+    }
+
+    if ($ehPrimo) {
+        echo $num . "\n";
+    }
+}
+?>
