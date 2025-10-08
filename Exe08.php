80d45bfec9d279cee477dee7743471b42f2bf471
Leonardo <leokenjits@gmail.com>
2025-10-08 20:02:02 -0300

    Add files via upload

---
+++ Exe08.php
@@ -0,0 +1,19 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 08 de Outubro de 2025
+Descritivo:WHILE: Faça um programa que imprima os múltiplos de 3, de 3 a 30.
+*******************************************************************************/
+$i = 3;
+while ($i <= 30){
+    echo $i."\n";
+    $i += 3;
+}
+?>
