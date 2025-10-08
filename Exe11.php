b9db1fac400c7ed034703df4453d16fd16b3a568
Leonardo <leokenjits@gmail.com>
2025-10-08 20:06:34 -0300

    Add files via upload

---
+++ Exe11.php
@@ -0,0 +1,19 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 08 de Outubro de 2025
+Descritivo:DO-WHILE: Escreva um programa que use do-while para imprimir os números de 1 a 10.
+*******************************************************************************/
+$i = 1;
+do {
+    echo $i."\n";
+    $i++;
+} while ($i <= 10);
+?>
