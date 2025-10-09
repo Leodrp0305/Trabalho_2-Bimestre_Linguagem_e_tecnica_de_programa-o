60312b554678acb5710e87f201eaeb1528cd3bf1
Lucas Coelho Suero <137530955+LucasSuero@users.noreply.github.com>
2025-10-09 20:06:44 -0300

    Add files via upload

---
+++ Exe16.php
@@ -0,0 +1,16 @@
+<?php 
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 08 de Outubro de 2025
+Descritivo:.Array Homogêneo: Crie um array com 5 nomes de cidades
+*******************************************************************************/
+$cit = ["Curitba","Tokyo", "São Paulo", "Paris", "Porto alegre"];
+    print_r($cit);
+?>
