8f12d74e41b2bb10d546d8702d64a830c98b41aa
Lucas Coelho Suero <137530955+LucasSuero@users.noreply.github.com>
2025-10-09 19:43:14 -0300

    Add files via upload

---
+++ Exe07.php
@@ -0,0 +1,28 @@
+<?php 
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 08 de Outubro de 2025
+Descritivo:.WHILE: Crie um programa que peça ao usuário para adivinhar um número (ex: 7) e continue pedindo até que ele acerte.
+*******************************************************************************/
+    $num = rand(0,10);
+    $i = 0;
+
+    while($num != $a ){ 
+        echo "Digite um número de 0 a 10: \n";
+        $a = trim(fgets(STDIN));
+        $i++;
+
+        
+    }
+
+    echo "O número era $num, você tomou $i tentativas para acertar."
+
+
+?>
