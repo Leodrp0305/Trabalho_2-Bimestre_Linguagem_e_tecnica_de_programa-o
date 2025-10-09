f0816cc6e91432ee16c1481e44eba9a259f725fb
Lucas Coelho Suero <137530955+LucasSuero@users.noreply.github.com>
2025-10-09 20:11:56 -0300

    Add files via upload

---
+++ Exe18.php
@@ -0,0 +1,19 @@
+<?php 
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 08 de Outubro de 2025
+Descritivo:FOREACH: Usando o array de cidades do exercício 16, imprima cada cidade em uma nova linha usando foreach.
+*******************************************************************************/
+$cit = ["Curitiba","Tokyo", "São Paulo", "Paris", "Porto alegre"];
+    foreach ($cit as $thisline) {
+        echo $thisline ."\n";
+    }
+
+?>
