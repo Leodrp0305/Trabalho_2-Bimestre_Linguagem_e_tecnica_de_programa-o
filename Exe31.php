74b3c9d4cb5c5fadbcd1e03656127e869e9acc67
Lucas Coelho Suero <137530955+LucasSuero@users.noreply.github.com>
2025-10-13 11:36:03 -0300

    Add files via upload

---
+++ Exe31.php
@@ -0,0 +1,21 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 08 de Outubro de 2025
+Descritivo: Procedimento: Crie um procedimento chamado saudacao que imprima "Bem-vindo ao PHP!"
+*******************************************************************************/
+
+    function saudacao(){ 
+        echo "Bem-vindo ao PHP!";
+    }
+    saudacao()
+
+
+?>
