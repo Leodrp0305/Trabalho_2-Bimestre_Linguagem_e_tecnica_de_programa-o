e7b9463cc3cdbcd33de5f9f542c54b5c64e1023e
Lucas Coelho Suero <137530955+LucasSuero@users.noreply.github.com>
2025-10-13 11:12:35 -0300

    Add files via upload

---
+++ Exe25.php
@@ -0,0 +1,19 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 08 de Outubro de 2025
+Descritivo: Array/Funções Nativas: Crie um array de números e use a função count() para mostrar quantos elementos ele possui
+*******************************************************************************/
+
+    $numeros = [ 5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+
+    echo "O array tem " . count($numeros) . " elementos";
+
+?>
