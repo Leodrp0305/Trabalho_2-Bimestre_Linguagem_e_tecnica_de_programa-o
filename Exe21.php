de067038c5d0294dda0803be089495ee015f44c5
Lucas Coelho Suero <137530955+LucasSuero@users.noreply.github.com>
2025-10-13 10:39:15 -0300

    Add files via upload

---
+++ Exe21.php
@@ -0,0 +1,23 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 08 de Outubro de 2025
+Descritivo: Array Heterogêneo (Associativo): Crie um array associativo para armazenar os dados de um aluno (nome, idade, curso).
+*******************************************************************************/
+
+    $aluno = [
+        "nome" => "Bruno", //string
+        "idade" =>"30",     //inteiro
+        "curso" => "Engenharia de Software", //string
+    ];
+
+    echo "Seu nome é: " . $aluno["nome"] . "\nVocê tem: " . $aluno["idade"] . " anos" . "\nSeu curso é: " . $aluno["curso"] ;  
+
+?>
