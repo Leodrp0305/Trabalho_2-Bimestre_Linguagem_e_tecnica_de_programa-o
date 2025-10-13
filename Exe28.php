6f881538e7f812ff00bb26aa42d03980c9a579f9
Lucas Coelho Suero <137530955+LucasSuero@users.noreply.github.com>
2025-10-13 11:22:12 -0300

    Add files via upload

---
+++ Exe28.php
@@ -0,0 +1,23 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 08 de Outubro de 2025
+Descritivo: Array/Busca: Crie um array de frutas. Escreva um código que verifique se a fruta "Maçã" existe no array.
+*******************************************************************************/
+
+    $frutas = ["Abacaxi", "Banana", "Mamão", "Maçã", "Laranja"];
+
+    if (in_array("Maçã", $frutas)) { // in_array verifica se o primeiro valor entre aspas esta presente no array frutas
+        echo "Maçã está no array";
+    } else {
+        echo "Maçã não está no array";
+    }
+
+?>
