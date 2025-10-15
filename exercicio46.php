ffca7d5684579f2849e0ad3ab7420e40dc9adeba
Lucas Coelho Suero <137530955+LucasSuero@users.noreply.github.com>
2025-10-15 20:57:13 -0300

    Update exercicio46.php

--- exercicio46.php
+++ exercicio46.php
@@ -11,24 +11,27 @@ Componentes:
 Data: 15 de Outubro de 2025
 Descritivo:Função: Função: Elabore uma função ehPrimo que receba um número e retorne true se ele for primo e false caso contrário.
 */
+   
+// Lê um número digitado pelo usuário e converte para inteiro
 $num = (int)readline("Digite um número:");
 
 function ehPrimo($numero) {
-   if ($numero <= 1) {
+   if ($numero <= 1) {// Números menores ou iguais a 1 não são primos
       return false;
    } else {
-      for ($i = 2; $i <= sqrt($numero); $i++) {
-         if ($numero%$i == 0) {
+      for ($i = 2; $i <= sqrt($numero); $i++) {// Testa divisores de 2 até a raiz quadrada do número
+         if ($numero % $i == 0) { // Se for divisível por algum número, não é primo
             return false;
-         } else {
-            return true;
-         }
+         } else {}
       }
    }
+  return true;
 } 
+// Chama a função e verifica o resultado
 if (ehPrimo($num) == true) {
    echo "$num é um número primo";
 } else {
    echo "$num não é um número primo";
 }
+
 ?>
