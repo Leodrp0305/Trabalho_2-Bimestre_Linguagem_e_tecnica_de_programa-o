ab9fd2f6ecce4aed61e2172856b222dbc14fdc22
Leonardo <leokenjits@gmail.com>
2025-10-20 15:31:02 -0300

    Update exercicio10.php

--- exercicio10.php
+++ exercicio10.php
@@ -11,12 +11,13 @@ Componentes:
 Data: 08 de Outubro de 2025
 Descritivo:WHILE: Crie um programa que conte quantos dígitos tem um número inteiro fornecido pelo usuário.
 *******************************************************************************/
-    echo "Digite um número: \n";
-    $var1 = (int)trim(fgets(STDIN));
-    $i = 0;
-    while ($i < strlen((string)$var1)) {
-        $i++;
+    echo "Digite um número: \n"; //instruimos o usuário a digitar um número.
+    $var1 = (int)trim(fgets(STDIN)); // pegamos o valor digitado pelo usuário e forçamos a ser inteiro
+    $i = 0; // iniciamos a variavel de contagem em 0 para poder contar apropriadamente
+    while ($i < strlen((string)$var1)) { //de forma bem redundante, utilizamos o strlen, para verificar a quantia de caracteres na string cedida para termos a quantia de repetições
+        $i++;//adicionamos 1 a variavel de contagem até atingir o valor obtido pelo strlen.
     }
-    echo $i;
+    echo $i; //imprimimos a variavel contadora
 
 ?>
+
