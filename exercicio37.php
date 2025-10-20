5c87b0b1a74e94f40248940dcb86bd272e3f9c02
Leonardo <leokenjits@gmail.com>
2025-10-20 15:55:19 -0300

    Update exercicio37.php

--- exercicio37.php
+++ exercicio37.php
@@ -11,14 +11,15 @@ Componentes:
 Data: 08 de Outubro de 2025
 Descritivo:Procedimento com Múltiplos Parâmetros: Crie um procedimento mostrarDados que receba nome, idade e cidade e os imprima de forma organizada
 *******************************************************************************/
-function mostrardados($nome, $idade,  $cidade){
-    echo "Seus dados pessoais são: \n";
-    echo "Seu nome é: " . $nome . "\n";
-    echo "Sua idade é: " . $idade . " anos \n";
-    echo "Sua cidade é: " . $cidade . "\n";
+function mostrardados($nome, $idade,  $cidade){ //criamos uma função chamada mostrardados com a função de formatar os dados que serão apresentados
+    echo "Seus dados pessoais são: \n"; //apresenta o que significa os dados abaixo
+    echo "Seu nome é: " . $nome . "\n"; //formatação do nome
+    echo "Sua idade é: " . $idade . " anos \n"; //formatação da idade
+    echo "Sua cidade é: " . $cidade . "\n"; //formatação da cidade
 }
     
-    mostrardados("Lucas", 18, "Rio de Janeiro")
+    mostrardados("Lucas", 18, "Rio de Janeiro")  //informações colocadas para retornar tudo formatado via função
 
 
 ?>
+
