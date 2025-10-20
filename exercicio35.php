daf6fae8c828fe5748628e7a73856c6258a0e068
Leonardo <leokenjits@gmail.com>
2025-10-20 15:38:43 -0300

    Update exercicio35.php

--- exercicio35.php
+++ exercicio35.php
@@ -11,18 +11,19 @@ Componentes:
 Data: 08 de Outubro de 2025
 Descritivo:Procedimento com Array: Crie um procedimento imprimirArray que receba um array e imprima todos os seus elementos.
 *******************************************************************************/
-$arrayqualquer = [
+$arrayqualquer = [ //criamos um array com o nome dos professores como uma menção honrosa.
  "José Carlos Domingues Flores",
  "Sidnei Martins de Jesus",
  "Bruno de sobrenome desconhecido",
  "Filipe Klos"
 ];
 
-function imprimirArray($array) {
-    foreach ($array as $thisLine) {
+function imprimirArray($array) { //iniciamos a funçãi imprimirArray como pedido com uma variavel padrão chamada $array
+    foreach ($array as $thisLine) { //aqui utilizamos um foreach para percorrer cada linha do array padrão, e abaixo, imprimir tal linha
         echo $thisLine."\n";
-    }
+    } //fechamos a funçãos
 
 }
-imprimirArray($arrayqualquer);
+imprimirArray($arrayqualquer); //aqui utilizamos a função criada para percorer o array criado na linha 14, o que significa que trocamos a variavel padrão pelo array entre parenteses 
 ?>
+
