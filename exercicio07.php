ece8751a4c32307b6957e4fc0b858f739ead003d
Lucas Coelho Suero <137530955+LucasSuero@users.noreply.github.com>
2025-10-20 14:29:05 -0300

    Update exercicio07.php

--- exercicio07.php
+++ exercicio07.php
@@ -11,19 +11,20 @@ Componentes:
 Data: 08 de Outubro de 2025
 Descritivo:.WHILE: Crie um programa que peça ao usuário para adivinhar um número (ex: 7) e continue pedindo até que ele acerte.
 *******************************************************************************/
-    $num = rand(0,10);
-    $i = 0;
+    $num = rand(0,10); // gera um número aleatório entre 0 e 10 e armazena na variável $num
+    $i = 0;  //$i vai contar quantas tentativas o usuário teve
 
-    while($num != $a ){ 
-        echo "Digite um número de 0 a 10: \n";
-        $a = trim(fgets(STDIN));
-        $i++;
+    while($num != $a ){ // enquanto o número digitado $a for diferente do número sorteado $num
+        echo "Digite um número de 0 a 10: \n"; //pede pro usuario digitar
+        $a = trim(fgets(STDIN)); // armazena oque o usuario digitar em $a 
+        $i++; //contador do numero de tentativas
 
         
     }
 
-    echo "O número era $num, você tomou $i tentativas para acertar."
+    echo "O número era $num, você tomou $i tentativas para acertar." // o laço é encerrado e esta mensagem aparece
 
 
 
 ?>
+
